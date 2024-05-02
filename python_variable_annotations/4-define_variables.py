9013930cc9832979218a0d38205787cd684a4fed
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-02 15:32:18 +0400

    Added task 5

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Type-annotated function define_variables that takes no arguments.
+    Returns three variables with specified values.
+"""
+
+
+def define_variables() -> tuple:
+    """Return a tuple of three variables."""
+    a: int = 1
+    b: float = 2.0
+    c: str = "string"
+    return a, b, c
