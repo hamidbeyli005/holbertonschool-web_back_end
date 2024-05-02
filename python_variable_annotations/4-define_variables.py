ac1b90539184ba3d0b1be4b66383652d9f5cc841
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-02 15:43:43 +0400

    Updated

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 """
-    Type-annotated function define_variables that takes no arguments.
-    Returns three variables with specified values.
+    Define variables with type annotations.
 """
 
-
-def define_variables() -> tuple:
-    """Return a tuple of three variables."""
-    a: int = 1
-    b: float = 2.0
-    c: str = "string"
-    return a, b, c
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
