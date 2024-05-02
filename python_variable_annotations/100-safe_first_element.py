8177dfa39b756190975f0bee3937eee386d0fb68
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-02 15:52:20 +0400

    Updated

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -3,10 +3,10 @@
     Type-annotated function safe_first_element that takes a sequence
     seq of any type and returns its first element.
 """
-from typing import Any, Sequence, Union
+from typing import Any, Sequence, Optional
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """ Gets first element safely.
     """
     if lst:
