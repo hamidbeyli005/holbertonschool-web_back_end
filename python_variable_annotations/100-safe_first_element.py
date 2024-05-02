a7fe51bca14a35a29ae310eb27178a587c2a417a
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-02 15:46:24 +0400

    Updated

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -3,10 +3,10 @@
     Type-annotated function safe_first_element that takes a sequence
     seq of any type and returns its first element.
 """
-from typing import Union, Any, Sequence
+from typing import Sequence, Optional, Any
 
 
-def safe_first_element(seq: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(seq: Sequence[Any]) -> Optional[Any]:
     """Return the first element of the sequence or None if empty."""
     if seq:
         return seq[0]
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-    Type-annotated function element_length that takes a list lst of strings
+    Type-annotated function element_length that takes a list of strings lst
     and returns a list of integers representing the lengths of the strings.
 """
-from typing import List
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: List[str]) -> List[int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of integers representing the lengths of the strings."""
-    return [len(i) for i in lst]
+    return [(i, len(i)) for i in lst]
