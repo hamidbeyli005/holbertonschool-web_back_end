21f289ab28a5cad3828e35eddf87643705b5a810
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-02 15:34:39 +0400

    Added task 9

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module contains a function that multiplies a float by a given number.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    This function returns a function that multiplies a float by a given number.
+    """
+    def multiply(number: float) -> float:
+        return number * multiplier
+    return multiply
