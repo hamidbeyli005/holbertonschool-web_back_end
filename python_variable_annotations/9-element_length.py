6f386ba6d2d2996eef3a896fd554d6eb1faec0b4
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-02 15:36:57 +0400

    Added task 10

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Type-annotated function element_length that takes a list lst of strings
+    and returns a list of integers representing the lengths of the strings.
+"""
+from typing import List
+
+
+def element_length(lst: List[str]) -> List[int]:
+    """Return a list of integers representing the lengths of the strings."""
+    return [len(i) for i in lst]
