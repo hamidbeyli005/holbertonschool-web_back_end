0feb53062727652268679883c993f1e0bffadc3a
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-02 15:20:41 +0400

    Added task 3

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Type-annotated function floor which takes a float n as argument
+    and returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n."""
+    return math.floor(n)
