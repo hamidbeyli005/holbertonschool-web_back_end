796b179924fc312750c72ede492f62e7a22cb9cf
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-08 21:59:29 +0400

    Updated

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,17 +4,15 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """ check for all elements in a collection """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    client = MongoClient('mongodb://localhost:27017')
+    collec = client.logs.nginx
 
-    print(f"{collection.estimated_document_count()} logs")
+    print(f'{collec.count_documents({})} logs')
+    print('Methods:')
 
-    print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        method_count = collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {method_count}")
-
-    check_get = collection.count_documents(
-        {'method': 'GET', 'path': "/status"})
-    print(f"{check_get} status check")
+    print(f'\tmethod GET: {collec.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {collec.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {collec.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {collec.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {collec.count_documents({"method": "DELETE"})}')
+    print(f'{collec.count_documents({"path": "/status"})} status check')
