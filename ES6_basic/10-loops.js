47d8f062131d5da5f9f41c3044e80a10877a4239
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-11 04:36:17 +0400

    Updated tasks

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // eslint-disable-next-line
-  for (const value of array) {
-    // eslint-disable-next-line
-    array[array.indexOf(value)] = appendString + value;
+  for (let value of array) {
+    value = appendString + value;
   }
 
   return array;
