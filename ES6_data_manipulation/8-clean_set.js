88bef2e6c6ac2980c5f0ca8b2d6939a29ed6ae7c
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-26 12:53:48 +0400

    Create cleanSet

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  const result = [];
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result.push(item.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
